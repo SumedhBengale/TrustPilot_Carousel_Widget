f2dc7da7015e7501e0bf44320e9b212768c5b2e6
Sumedh Bengale <ssbengale@gmail.com>
2023-08-06 01:54:14 +0530

    Fix for S3 Compatibility

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const reviews_url ='https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/reviews.json';
-const star = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star.svg';
-const star_green = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_green.svg';
-const star_grey = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_grey.svg';
-const star_light = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_light.svg';
+const reviews_url ='https://trustpilot-bucket.s3.eu-west-2.amazonaws.com/reviews.json';
+const star = 'https://trustpilot-bucket.s3.eu-west-2.amazonaws.com/star.svg';
+const star_green = 'https://trustpilot-bucket.s3.eu-west-2.amazonaws.com/star_green.svg';
+const star_grey = 'https://trustpilot-bucket.s3.eu-west-2.amazonaws.com/star_grey.svg';
+const star_light = 'https://trustpilot-bucket.s3.eu-west-2.amazonaws.com/star_light.svg';
