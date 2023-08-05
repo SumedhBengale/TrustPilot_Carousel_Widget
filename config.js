ffefd403811b9d5e8fc4f1661bbabbe6196e7826
Sumedh Bengale <ssbengale@gmail.com>
2023-08-05 14:43:26 +0530

    Carousel Widget Implemented

---
+++ config.js
@@ -0,0 +1,5 @@
+const reviews_url ='https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/reviews.json';
+const star = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star.svg';
+const star_green = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_green.svg';
+const star_grey = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_grey.svg';
+const star_light = 'https://raw.githubusercontent.com/SumedhBengale/jsonHost/master/star_light.svg';
